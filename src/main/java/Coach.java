29da89b420138f3e722c9e26e14a63ec6e388755
JianHao24 <cheongjianhao@gmail.com>
2025-08-28 01:17:28 +0800

    Added echo and exit function when user types bye

--- src/main/java/Coach.java
+++ src/main/java/Coach.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Coach {
+
+
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Coach");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = in.nextLine().trim();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            else {
+
+                System.out.println("____________________________________________________________");
+                System.out.println(input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+        in.close();
+
     }
 }
+
