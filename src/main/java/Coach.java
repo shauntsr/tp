7555543c9b1a01bfeac29f11c9315a03d45af226
JIANHAO\cheon <cheongjianhao@gmail.com>
2025-08-28 11:34:22 +0800

    Added new class Task and ability to mark tasks finished or unmark them as unfinished

--- src/main/java/Coach.java
+++ src/main/java/Coach.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
 
 public class Coach {
-    static String[] tasks = new String[100];
+    static Task[] tasks = new Task[100];
     static int taskCount = 0;
+
+    private static String printTask(Task task) {
+        String mark = task.isDone() ? "[X]" : "[ ]";
+        return mark + " " + task.getName();
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,25 +19,39 @@ public class Coach {
 
         while (true) {
             String input = sc.nextLine().trim();
+            String[] inputs = input.split(" ");
 
-            if (input.equalsIgnoreCase("bye")) {
+            if (inputs[0].equalsIgnoreCase("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            }
-            else if (input.equalsIgnoreCase("list")) {
+            } else if (inputs[0].equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + ". " + printTask(tasks[i]));
                 }
                 if (taskCount == 0) {
                     System.out.println(" No tasks yet!");
                 }
                 System.out.println("____________________________________________________________");
-            }
-            else {
-                tasks[taskCount] = input;
+            } else if (inputs[0].equalsIgnoreCase("mark")) {
+                System.out.println("____________________________________________________________");
+                int index = Integer.parseInt(inputs[1]) - 1;
+                tasks[index].setDone(true);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(" " + printTask(tasks[index]));
+                System.out.println("____________________________________________________________");
+            } else if (inputs[0].equalsIgnoreCase("unmark")) {
+                System.out.println("____________________________________________________________");
+                int index = Integer.parseInt(inputs[1]) - 1;
+                tasks[index].setDone(false);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(" " + printTask(tasks[index]));
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = new Task(input, false);
                 taskCount++;
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + input);
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String name;
+    private boolean isDone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public Task(String name, boolean isDone) {
+        this.name = name;
+        this.isDone = isDone;
+    }
+}
