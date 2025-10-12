bf754951bb36ef17fb3f9b3c2edf139a257cd9d0
JianHao24 <cheongjianhao@gmail.com>
2025-10-12 16:42:33 +0800

    Removed ascii art

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -7,12 +7,7 @@ public class Duke {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ______    _   _       _  ____ _     ___ \n" +
-                "|__  / ___| |_| |_ ___| |/ ___| |   |_ _|\n" +
-                "  / / / _ \\ __| __/ _ \\ | |   | |    | | \n" +
-                " / /_|  __/ |_| ||  __/ | |___| |___ | | \n" +
-                "/____|\\___|\\___|\\__\\___|_|\\____|_____|___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from ZettelCLI");
         System.out.println("What is your name?");
 
         Scanner in = new Scanner(System.in);
