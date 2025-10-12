53eec9179b7ae6c151b742fdd954197b14fd9384
JianHao24 <cheongjianhao@gmail.com>
2025-10-12 15:40:01 +0800

    Added basic greeting and J-Unit testing

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -7,11 +7,11 @@ public class Duke {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ______    _   _       _  ____ _     ___ \n" +
+                "|__  / ___| |_| |_ ___| |/ ___| |   |_ _|\n" +
+                "  / / / _ \\ __| __/ _ \\ | |   | |    | | \n" +
+                " / /_|  __/ |_| ||  __/ | |___| |___ | | \n" +
+                "/____|\\___|\\___|\\__\\___|_|\\____|_____|___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
 
--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -1,12 +1,45 @@
 package seedu.duke;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+public class DukeTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final ByteArrayInputStream inContent = new ByteArrayInputStream("John".getBytes());
+    private final String logo = " ______    _   _       _  ____ _     ___ \n" +
+            "|__  / ___| |_| |_ ___| |/ ___| |   |_ _|\n" +
+            "  / / / _ \\ __| __/ _ \\ | |   | |    | | \n" +
+            " / /_|  __/ |_| ||  __/ | |___| |___ | | \n" +
+            "/____|\\___|\\___|\\__\\___|_|\\____|_____|___|\n";
 
-class DukeTest {
-    @Test
-    public void sampleTest() {
-        assertTrue(true);
+    @BeforeEach //set up environment for test
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setIn(inContent);
+    }
+
+    @AfterEach //reset environment after test
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setIn(System.in);
+    }
+
+    @Test //test if print ascii and correct name
+    public void testMainOutput() {
+        Duke.main(new String[]{});
+        String output = outContent.toString();
+
+        assertTrue(output.contains("Hello from"));
+        assertTrue(output.contains(logo));
+        assertTrue(output.contains("What is your name?"));
+        assertTrue(output.contains("Hello John"));
     }
 }
