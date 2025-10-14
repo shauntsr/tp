4102e9d8c92d05730e688b9321d3f0dfbe654905
gordonajajar <178007695+gordonajajar@users.noreply.github.com>
2025-10-14 14:22:33 +0800

    Refactor: remove redundant code from Parser, Storage, UI, Zettel

--- src/main/java/seedu/zettel/Zettel.java
+++ src/main/java/seedu/zettel/Zettel.java
@@ -7,10 +7,5 @@ public class Zettel {
      * Main entry-point for the java.zettel.Zettel application.
      */
     public static void main(String[] args) {
-        System.out.println("Hello from ZettelCLI");
-        System.out.println("What is your name?");
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
     }
 }
