9f388a53a74e6fc7a4b5d56dd7159830529f4ecd
gordonajajar <178007695+gordonajajar@users.noreply.github.com>
2025-10-14 14:25:53 +0800

    Refactor: add skeleton Note class

--- src/main/java/seedu/zettel/commands/Command.java
+++ src/main/java/seedu/zettel/commands/Command.java
@@ -1,14 +1,12 @@
 package seedu.zettel.commands;
 
-import seedu.zettel.tasks.Task;
 import seedu.zettel.UI;
 import seedu.zettel.Storage;
-import seedu.zettel.exceptions.CoachException;
 
 import java.util.ArrayList;
 
 public abstract class Command {
-    public abstract void execute(ArrayList<Task> tasks, UI ui, Storage storage) throws CoachException;
+    public abstract void execute(ArrayList<Note> notes, UI ui, Storage storage);
 
     public boolean isExit() {
         return false;
