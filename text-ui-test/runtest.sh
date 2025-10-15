bf2a237ffa38776e9a2365ab56968a5b3c52016f
Ksheerabthi Nathan <vksheerabthi@gmail.com>
2025-10-15 11:10:34 +0800

    Update text-ui-test

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -8,11 +8,22 @@ cd ..
 
 cd text-ui-test
 
-java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
+java -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
+
+# Normalize IDs - replace all 8-character hex IDs with a placeholder
+sed -E 's/#[a-f0-9]{8}/#XXXXXXXX/g' ACTUAL.TXT > ACTUAL-NORMALIZED.TXT
+sed -E 's/[a-f0-9]{8}( |$)/XXXXXXXX\1/g' ACTUAL-NORMALIZED.TXT > ACTUAL-NORMALIZED2.TXT
+cp ACTUAL-NORMALIZED2.TXT ACTUAL-NORMALIZED.TXT
 
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix EXPECTED-UNIX.TXT ACTUAL.TXT
-diff EXPECTED-UNIX.TXT ACTUAL.TXT
+dos2unix EXPECTED-UNIX.TXT ACTUAL-NORMALIZED.TXT
+
+# Normalize the expected file too
+sed -E 's/#[a-f0-9]{8}/#XXXXXXXX/g' EXPECTED-UNIX.TXT > EXPECTED-NORMALIZED.TXT
+sed -E 's/[a-f0-9]{8}( |$)/XXXXXXXX\1/g' EXPECTED-NORMALIZED.TXT > EXPECTED-NORMALIZED2.TXT
+cp EXPECTED-NORMALIZED2.TXT EXPECTED-NORMALIZED.TXT
+
+diff EXPECTED-NORMALIZED.TXT ACTUAL-NORMALIZED.TXT
 if [ $? -eq 0 ]
 then
     echo "Test passed!"
