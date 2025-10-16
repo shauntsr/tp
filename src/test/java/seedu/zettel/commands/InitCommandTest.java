8d42a60fccec14d3d6744d6fc35202da5c1d54ed
Shaun Tan <shauntansr@gmail.com>
2025-10-16 18:13:23 +0800

    Add tests for InitCommand

---
+++ src/test/java/seedu/zettel/commands/InitCommandTest.java
@@ -0,0 +1,38 @@
+package seedu.zettel.commands;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.zettel.UI;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class InitCommandTest {
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final PrintStream originalOutputStream = System.out;
+
+    private UI ui;
+
+    @BeforeEach
+    void setup() {
+        System.setOut(new PrintStream(outputStream));
+        ui = new UI();
+    }
+
+    @AfterEach
+    void teardown() {
+        System.setOut(originalOutputStream);
+    }
+
+    @Test
+    void show_repo_init_prints_correct_message() {
+        String repoName = "myRepo";
+        ui.showRepoInit(repoName);
+
+        String output = outputStream.toString().trim();
+        assertTrue(output.contains("Repository /" + repoName + " has been created."));
+    }
+}
