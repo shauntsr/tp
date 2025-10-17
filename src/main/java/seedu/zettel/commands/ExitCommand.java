4e7e2b0e31b728b40518dcecdd07aac789eb97a4
gordonajajar <178007695+gordonajajar@users.noreply.github.com>
2025-10-17 16:58:47 +0800

    Add JavaDoc to ExitCommand

--- src/main/java/seedu/zettel/commands/ExitCommand.java
+++ src/main/java/seedu/zettel/commands/ExitCommand.java
@@ -7,12 +7,27 @@ import seedu.zettel.UI;
 
 import java.util.ArrayList;
 
+/**
+ * Command to exit the Zettel application.
+ * An ExitCommand displays a goodbye message and signals the application to terminate.
+ */
 public class ExitCommand extends Command {
+
+    /**
+     * Executes the exit command by displaying a goodbye message to the user.
+     *
+     * @param ui The UI object for displaying the goodbye message.
+     */
     @Override
     public void execute(ArrayList<Note> notes, UI ui, Storage storage) throws ZettelException {
         ui.showBye();
     }
 
+    /**
+     * Indicates that this command should terminate the application.
+     *
+     * @return True, as this command exits the application.
+     */
     @Override
     public boolean isExit() {
         return true;
